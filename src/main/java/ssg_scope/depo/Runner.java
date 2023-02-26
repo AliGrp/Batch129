b315327e8d837a9c669b5fda23d578d59319ec6b
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-02-26 16:09:47 +0300

    v1

---
+++ src/main/java/ssg_scope/depo/Runner.java
@@ -0,0 +1,15 @@
+package ssg_scope.depo;
+
+import static exercise.depo.Depo.menu;
+import static exercise.depo.Depo.urun;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        urun();
+        menu();
+
+    }
+
+
+}
